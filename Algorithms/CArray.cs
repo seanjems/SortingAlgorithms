2a3e35c154e34001f6eeeb22bbb9189ef632d777
Najuna James <seanjems@gmail.com>
2021-12-27 18:26:41 +0300

    Add project files.

---
+++ Algorithms/CArray.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class CArray
+    {
+        private int[] arr;
+        private int numElements;
+        private int upper;
+
+        public CArray(int size)
+        {
+            arr = new int[size];
+            upper = size - 1;
+            numElements = 0;
+        }
+        public void Insert(int item)
+        {
+            arr[numElements] = item;
+            numElements++;
+        }
+        public void DisplayElements()
+        {
+            for (int i = 0; i <= upper; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+        public void Clear()
+        {
+            for (int i = 0; i <=upper; i++)
+            {
+                arr[i] = 0;
+                numElements = 0;
+            }
+        }
+        public  void BubbleSort()
+        {
+            int temp;
+            for (int outer= upper; outer >= 1; outer--)
+            {
+                for (int inner = 0; inner <= outer-1; inner++)
+                {
+                    if (arr[inner]>arr[inner+1])
+                    {
+                        temp = arr[inner];
+                        arr[inner] = arr[inner + 1];
+                        arr[inner + 1] = temp;
+                    }
+                    Console.WriteLine("");
+                    this.DisplayElements();
+                }
+                
+            }
+        }
+        public void SelectionSort()
+        {
+            int temp, min;
+            for (int outer = 0; outer <= upper; outer++)
+            {
+                min = outer;
+                for (int inner = outer+1; inner <=outer; inner++)
+                {
+                    if (arr[inner]<arr[outer])
+                    {
+                        min = inner;
+                    }
+                    temp = arr[outer];
+                    arr[inner] = arr[min];
+                    arr[min] = temp;
+
+                    Console.WriteLine("");
+                    this.DisplayElements();
+                }
+            }
+        }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CArray nums = new CArray(10);
+            Random rnd = new Random(100);
+            for (int i = 0; i < 10; i++)
+                nums.Insert((int)(rnd.NextDouble() * 100));
+            Console.WriteLine("Before sorting: ");
+            nums.DisplayElements();
+            Console.WriteLine("During sorting: ");
+            nums.SelectionSort();
+            Console.WriteLine("After sorting: ");
+            nums.DisplayElements();
+
+
+        }
+       
+    }
+}
